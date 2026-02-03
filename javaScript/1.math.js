d60b3c386c81c057f7566a484a49e251a7a522aa
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-03 17:36:04 +0530

    compare three number usnig if-else  and return the bigger one

--- javaScript/1.math.js
+++ javaScript/1.math.js
@@ -4,7 +4,6 @@ const nums1 = 34;
 const nums2 = 45;
 
 function biggestNum(a, b) {
-    console.log(a, b);
     if (a > b) {
         return a;
     } else {
@@ -13,3 +12,26 @@ function biggestNum(a, b) {
 }
 const bigNumber = biggestNum(nums1, nums2);
 console.log(bigNumber);
+
+
+
+// compare three number using if else
+const number1 = 85;
+const number2 = 95;
+const number3 = 66;
+
+function highestNum(nums1, nums2, nums3) {
+    if (nums1 > nums2 && nums1 > nums3) {
+        return nums1;
+    } else if (nums2 > nums1 && nums2 > nums3) {
+        return nums2;
+    } else {
+        return nums3;
+    }
+
+}
+
+const biggerNum = highestNum(number1, number2, number3);
+console.log(biggerNum);
+
+
