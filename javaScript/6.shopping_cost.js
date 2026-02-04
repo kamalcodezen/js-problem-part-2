a85bd2f8e880ffb4a4d7d1bc136bddc60cf4ca6d
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-04 05:50:38 +0530

    array inside object value acces and sum calculate and quantity multiply

--- javaScript/6.shopping_cost.js
+++ javaScript/6.shopping_cost.js
@@ -14,3 +14,27 @@ function totalProductsPrice(products) {
 
 const total = totalProductsPrice(products);
 console.log(total);
+
+
+console.log(`------------------`);
+
+
+const items = [
+    { name: "pant", price: 200, quantity: 1 },
+    { name: "shirt", price: 100, quantity: 1 },
+    { name: "shoes", price: 100, quantity: 1 }
+];
+
+
+function itemsQuantityCount(items) {
+    let total = 0;
+    for (let item of items) {
+        console.log(item);
+        let totalCost = item.price * item.quantity;
+        total = total + totalCost;
+    }
+    return total;
+}
+
+const result = itemsQuantityCount(items);
+console.log(result);    
