10080f5591b2ae4cc79faded3b3924bdfa793781
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-10 17:49:19 +0530

    total find quantity acces

--- again-practice-javaScript/total.js
+++ again-practice-javaScript/total.js
@@ -13,4 +13,26 @@ function totalCost(pantQuantity, shirtQuantity, shoesQuantity) {
 console.log(totalCost(2, 3, 2));
 
 
+console.log(`----------------------`);
+
+
+
+const items = [
+    { name: "shirt", price: 800, quantity: 3 },
+    { name: "pant", price: 900, quantity: 2 },
+    { name: "shoes", price: 700, quantity: 2 },
+
+];
+
+function totalNewCost(items) {
+    let total = 0;
+    for (let item of items) {
+        let price = item.price * item.quantity;
+        total = total + price;
+    }
+    return total
+}
+console.log(totalNewCost(items));
+
+
 
